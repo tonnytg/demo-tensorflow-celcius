a8dd56031cc1a0daff1dcea2d0520531d969c55e
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2024-07-01 23:46:16 -0300

    fix: improve code
    test with tensorflow

--- demo2.py
+++ demo2.py
@@ -7,36 +7,42 @@ celsius_q = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
 # Dados de saída (Fahrenheit)
 fahrenheit_a = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 
-# Definindo o modelo
+# Definindo o modelo com uma camada densa
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(units=1, input_shape=[1])
 ])
 
-# Compilando o modelo
+# Compilando o modelo com otimizador Adam e perda de erro quadrático médio
 model.compile(optimizer=tf.keras.optimizers.Adam(0.1), loss='mean_squared_error')
 
 # Treinando o modelo
-history = model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=False)
+history = model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=0)
 
 # Fazendo previsões
 predicted_fahrenheit = model.predict(celsius_q)
 
-# Plotando os dados originais e as previsões
-plt.figure(figsize=(10, 6))
-plt.plot(celsius_q, fahrenheit_a, 'ro', label='Dados Reais')
-plt.plot(celsius_q, predicted_fahrenheit, 'b-', label='Previsões do Modelo')
-plt.xlabel('Celsius')
-plt.ylabel('Fahrenheit')
-plt.title('Conversão de Celsius para Fahrenheit')
-plt.legend()
-plt.grid(True)
-plt.show()
+# Função para plotar os dados reais e as previsões do modelo
+def plot_predictions(celsius, real_fahrenheit, predicted_fahrenheit):
+    plt.figure(figsize=(10, 6))
+    plt.plot(celsius, real_fahrenheit, 'ro', label='Dados Reais')
+    plt.plot(celsius, predicted_fahrenheit, 'b-', label='Previsões do Modelo')
+    plt.xlabel('Celsius')
+    plt.ylabel('Fahrenheit')
+    plt.title('Conversão de Celsius para Fahrenheit')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
-# Plotando a perda ao longo das épocas
-plt.figure(figsize=(10, 6))
-plt.xlabel('Número de Épocas')
-plt.ylabel('Magnitude da Perda')
-plt.plot(history.history['loss'])
-plt.title('Perda durante o Treinamento')
-plt.grid(True)
-plt.show()
+# Função para plotar a perda ao longo das épocas
+def plot_loss(history):
+    plt.figure(figsize=(10, 6))
+    plt.plot(history.history['loss'])
+    plt.xlabel('Número de Épocas')
+    plt.ylabel('Magnitude da Perda')
+    plt.title('Perda durante o Treinamento')
+    plt.grid(True)
+    plt.show()
+
+# Chamando as funções de plotagem
+plot_predictions(celsius_q, fahrenheit_a, predicted_fahrenheit)
+plot_loss(history)
